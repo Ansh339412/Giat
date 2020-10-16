e27f68d2898f32c8ac7b7ab3eebfd65b22514f40
Ansh339412 <hakikatsingh099@gmail.com>
2020-10-16 18:46:45 +0530

    changed canvas size

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function preload() {
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    var canvas = createCanvas(1100,400);
     engine = Engine.create();
     world = engine.world;
 
